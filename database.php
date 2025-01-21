e9ce46b49ab7adc3c8273401d275620f0d18e903
Keyner-Mena <keynermena896@gmail.com>
2025-01-21 14:01:33 -0600

    Version 1

---
+++ database.php
@@ -0,0 +1,11 @@
+<?php
+    $connection = mysqli_connect(
+        '127.0.0.1:7703', // Server Name (puede ser cambiado a localhost u otro)
+        'root', // Username
+        '', // Password
+        'task-app' // Database Name
+    );
+    /*if ($connection){
+        echo "Conectado";
+    }*/
+?>
